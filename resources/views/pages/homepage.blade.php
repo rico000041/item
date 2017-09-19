e80063199dadb043dc5ac046472cefdda70f4c5d
= <31499631+rico000041@users.noreply.github.com>
2017-09-19 16:54:25 -0700

    Something change

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -14,19 +14,19 @@
                         
                         <div class="navbar-header">
                            
-                            <a class="navbar-brand" href="/main/electronics">
+                            <a class="navbar-brand" href="#">
                                 Electronics
                             </a>
-                            <a class="navbar-brand" href="/main/furnitures">
+                            <a class="navbar-brand" href="#">
                                 Furnitures
                             </a>
-                            <a class="navbar-brand" href="/main/clothes">
+                            <a class="navbar-brand" href="#">
                                 Clothes
                             </a>
-                            <a class="navbar-brand" href="/main/toys">
+                            <a class="navbar-brand" href="#">
                                 Toys
                             </a>
-                            <a class="navbar-brand" href="/main/footwear">
+                            <a class="navbar-brand" href="#">
                                 Footwear
                             </a>
 
@@ -46,7 +46,7 @@
                         <center>
                             @foreach($posts as $post)
                             <a href = '/{{$post->id}}'>
-                                <img src="uploads/{{$post->file}}" style = 'width:150px; height:150px;' width = '100%' height = '100%'><br/>
+                                <img src="uploads/{{$post->file}}" style = 'width:200px; height:200px;' width = '100%' height = '100%'><br/>
                                 </a>
 
                                 <p> {{$post->name}}</p>
